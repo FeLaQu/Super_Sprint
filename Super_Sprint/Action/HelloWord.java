e147bd596d45ba8a4a06e195ec09b57786be1277
fengchengang <chengang.feng@supelec.fr>
2014-12-03 13:34:26 +0100

    delete 2

--- Super_Sprint/Action/HelloWord.java
+++ Super_Sprint/Action/HelloWord.java
@@ -2,7 +2,8 @@ public class HelloWord {
 
 	public static void main(String args[])
 	{ 
-	System.out.println("Hello  World2!"); 
+	System.out.println("Hello  World!"); 
+	//delete 2
 	System.out.println("Hello  Lady!"); 
 	} 
 	
