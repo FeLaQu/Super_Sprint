50da2ffd1ab4034f9155e22962b901471e0673b4
Laurent <laurent.gaujac@supelec.fr>
2014-12-03 15:29:34 +0100

    YOLES PD

--- Super_Sprint/Action/HelloWord.java
+++ Super_Sprint/Action/HelloWord.java
@@ -3,7 +3,7 @@ public class HelloWord {
 	public static void main(String args[])
 	{ 
 	System.out.println("Hello  World!"); 
-	//delete 2
+	//delete 2NIKSARACE
 	System.out.println("Hello  Lady!"); 
 	} 
 	
