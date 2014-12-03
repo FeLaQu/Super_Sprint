432631a19600b3acfd6e186c860302384f4735a1
Laurent <laurent.gaujac@supelec.fr>
2014-12-03 15:34:16 +0100

    J'ai enlevé les niksarace by Laurent

--- Super_Sprint/Action/HelloWord.java
+++ Super_Sprint/Action/HelloWord.java
@@ -3,7 +3,7 @@ public class HelloWord {
 	public static void main(String args[])
 	{ 
 	System.out.println("Hello  World!"); 
-	//delete 2NIKSARACE
+	//delete 2
 	System.out.println("Hello  Lady!"); 
 	} 
 	
