f7a3863180e3af1dedfa8a1206d8e930404f58ad
Laurent <laurent.gaujac@supelec.fr>
2014-12-05 11:39:35 +0100

    Un début de classe Car

--- Super_Sprint/Modele/Car.java
+++ Super_Sprint/Modele/Car.java
@@ -2,8 +2,17 @@ package Modele;
 
 public class Car {
 
+	private int ID ;		//Identification of the car
+	private int[] position ; //A 2x1 vector for the current position
+	private int[] speed ;  //IDEM 
+	private int[] acceleration ; // IDEM
+	private int max_speed ;		// Characteristic of the car
+	private int max_acceleration ;	//IDEM
+	private float maniability ; //IDEM
+	
+	
 	public Car() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
